7e21870bcb4d83446ce1ea34ca9e677fcb29c972
Hamed Taherkhani <hamedtahi7@gmail.com>
2022-07-30 15:11:36 +0430

    make BlocksList clean

--- src/Components/BlocksList.js
+++ src/Components/BlocksList.js
@@ -5,61 +5,77 @@ import Sponsors from "./Sponsors";
 import PeopleComments from "./PeopleComments";
 
 function BlocksList() {
+  const blockData = [
+    {
+      title: "Products",
+      description: (
+        <>
+          Every component you need is <strong>ready for production</strong>
+        </>
+      ),
+      summary:
+        "Build at an accelerated pace without sacrificing flexibility or control.",
+    },
+    {
+      title: "Why build with MUI ?",
+      description: (
+        <>
+          A <strong>delightful</strong> experience for you and your users
+        </>
+      ),
+      bg: "#001e3c",
+      children: <MUIFeatures />,
+    },
+    {
+      title: "Production-ready components",
+      description: (
+        <>
+          Beautiful and powerful, <strong>right out of the box</strong>
+        </>
+      ),
+      bg: "#001e3c",
+    },
+    {
+      bg: "#132f4c",
+      children: <PeopleComments />,
+    },
+    {
+      title: "Sponsors",
+      description: (
+        <>
+          <strong>You</strong> make this possible
+        </>
+      ),
+      summary:
+        "The development of these open-source tools is accelerated by out generous sponsors.",
+      bg: "#001e3c",
+      children: <Sponsors />,
+    },
+    {
+      title: "Start now",
+      description: (
+        <>
+          Ship your next project <strong>faster</strong>
+        </>
+      ),
+      summary:
+        "Find out why MUI's tools are trusted by thousands of open source developers and teams around the world.",
+    },
+  ];
+
   return (
     <div className="blocks">
-      <Block
-        title="Products"
-        description={
-          <>
-            Every component you need is <strong>ready for production</strong>
-          </>
-        }
-        summary="Build at an accelerated pace without sacrificing flexibility or control."
-      ></Block>
-      <Block
-        title="Why build with MUI ?"
-        description={
-          <>
-            A <strong>delightful</strong> experience for you and your users
-          </>
-        }
-        bg="#001e3c"
-      >
-        <MUIFeatures />
-      </Block>
-      <Block
-        title="Production-ready components"
-        description={
-          <>
-            Beautiful and powerful, <strong>right out of the box</strong>
-          </>
-        }
-        bg="#001e3c"
-      ></Block>
-      <Block bg="#132f4c">
-        <PeopleComments />
-      </Block>
-      <Block
-        title="Sponsors"
-        description={
-          <>
-            <strong>You</strong> make this possible
-          </>
-        }
-        summary="The development of these open-source tools is accelerated by out generous sponsors."
-        bg="#001e3c"
-      >
-        <Sponsors />
-      </Block>
-      <Block
-        title="Start now"
-        description={
-          <>
-            Ship your next project <strong>faster</strong>
-          </>
-        }
-        summary="Find out why MUI's tools are trusted by thousands of open source developers and teams around the world."
-      ></Block>
+      {blockData.map(({ title, description, summary, bg, children }, i) => (
+        <Block
+          key={i}
+          title={title}
+          description={description}
+          summary={summary}
+          bg={bg}
+        >
+          {children}
+        </Block>
+      ))}
     </div>
   );
 }
