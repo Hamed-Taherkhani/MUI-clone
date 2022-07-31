f01f73498baa5272867056d94aebe6585c6fa7cc
Hamed Taherkhani <hamedtahi7@gmail.com>
2022-07-31 17:45:31 +0430

    Add clipboard copy functionality to 'npm install' btn of GetStarted component

--- src/Components/GetStarted.js
+++ src/Components/GetStarted.js
@@ -1,9 +1,38 @@
-import { Box, Button, Typography } from "@mui/material";
+import {
+  Alert,
+  AlertTitle,
+  Box,
+  Button,
+  IconButton,
+  Snackbar,
+  Typography,
+} from "@mui/material";
 import "./css/GetStarted.css";
 import NavigateNextRoundedIcon from "@mui/icons-material/NavigateNextRounded";
 import CopyAllRoundedIcon from "@mui/icons-material/CopyAllRounded";
+import DoneRoundedIcon from "@mui/icons-material/DoneRounded";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import { useState } from "react";
 
 function GetStarted() {
+  const [isCopied, setIsCopied] = useState(false);
+  const [isSnackOpen, setIsSnackOpen] = useState(false);
+
+  const copyDataToClipboard = (e) => {
+    navigator.clipboard
+      .writeText("npm install @mui/material @emotion/react @emotion/styled")
+      .then(() => {
+        setIsCopied(true);
+
+        setTimeout(() => {
+          setIsCopied(false);
+        }, 2000);
+      })
+      .catch(() => {
+        setIsSnackOpen(true);
+      });
+  };
+
   return (
     <Box
       className="Get-started-root"
@@ -12,13 +41,40 @@ function GetStarted() {
       gap={1}
       sx={{ Button: { textTransform: "none", color: "#fff" } }}
     >
+      <Snackbar
+        autoHideDuration={6000}
+        open={isSnackOpen}
+        onClose={() => setIsSnackOpen(false)}
+      >
+        <Alert
+          severity="error"
+          title=""
+          action={
+            <IconButton onClick={() => setIsSnackOpen(false)}>
+              <CloseRoundedIcon sx={{ color: "#5f2120" }} />
+            </IconButton>
+          }
+        >
+          <AlertTitle>Was not copied !!</AlertTitle>
+          Please make sure you allow{" "}
+          <span style={{ fontWeight: "700" }}>clipboard permission</span>.
+        </Alert>
+      </Snackbar>
+
       <Button endIcon={<NavigateNextRoundedIcon />} variant="contained">
         Get started
       </Button>
 
       <Button
         variant="outlined"
-        endIcon={<CopyAllRoundedIcon sx={{ color: "#fff" }} />}
+        onClick={copyDataToClipboard}
+        endIcon={
+          isCopied ? (
+            <DoneRoundedIcon sx={{ color: "rgb(51, 153, 255)" }} />
+          ) : (
+            <CopyAllRoundedIcon sx={{ color: "#fff" }} />
+          )
+        }
         sx={{
           display: "flex",
           justifyContent: "space-between",
